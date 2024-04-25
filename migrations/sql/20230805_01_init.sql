552eb2a14924209ad42dff661ca567fff9e401b9
kanazawa-n <n.kanazawa.work@gmail.com>
2024-04-26 08:48:51 +0900

    fix init db

--- migrations/sql/20230805_01_init.sql
+++ migrations/sql/20230805_01_init.sql
@@ -9,23 +9,29 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB COMMENT='user table';
 
+CREATE TABLE IF NOT EXISTS `roast_levels` (
+  `id` SERIAL PRIMARY KEY COMMENT 'roast id',
+  `level` SMALLINT NOT NULL COMMENT '焙煎度合い',
+  `name` VARCHAR(255) NOT NULL COMMENT '名前'
+) ENGINE=InnoDB COMMENT='roast level table';
+
 CREATE TABLE IF NOT EXISTS `beans` (
-  `id` VARCHAR(255) NOT NULL COMMENT 'beans id',
-  `user_id` VARCHAR(255) COMMENT 'user name',
+  `id` SERIAL PRIMARY KEY COMMENT 'beans id',
+  `user_id` VARCHAR(255) COMMENT 'user id',
+  `roast_id` INT NOT NULL COMMENT 'roast id',
   `production_area` VARCHAR(255) NOT NULL COMMENT '産地',
   `kind` VARCHAR(255) NOT NULL COMMENT '品種',
-  `roast_level` VARCHAR(255) NOT NULL COMMENT '焙煎度合い',
-  `price` VARCHAR(255) NOT NULL COMMENT '購入単価',
-  `gram` VARCHAR(255) NOT NULL COMMENT '購入グラム',
+  `price` SMALLINT NOT NULL COMMENT '購入価格',
+  `gram` SMALLINT NOT NULL COMMENT '購入グラム',
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'create_date',
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update_date',
   `deleted_at` TIMESTAMP NULL DEFAULT NULL COMMENT 'delete_date',
-  PRIMARY KEY (`id`),
   FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB COMMENT='bean table';
 
 CREATE TABLE IF NOT EXISTS `recipes` (
-  `id` VARCHAR(255) NOT NULL COMMENT 'recipes id',
+  `id` SERIAL PRIMARY KEY NOT NULL COMMENT 'recipes id',
+  `bean_id` INT NOT NULL COMMENT 'bean id',
   `user_id` VARCHAR(255) NOT NULL COMMENT 'user id',
   `step_number` VARCHAR(255) NOT NULL COMMENT 'ステップ数',
   `memo` VARCHAR(255) NOT NULL COMMENT 'メモ',
@@ -34,11 +40,11 @@ CREATE TABLE IF NOT EXISTS `recipes` (
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'create_date',
   `updated_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'update_date',
   `deleted_at` TIMESTAMP NULL DEFAULT NULL COMMENT 'delete_date',
-  PRIMARY KEY (`id`),
   FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB COMMENT='recipe table';
 
 -- +migrate Down
 DROP TABLE IF EXISTS `recipes`;
+DROP TABLE IF EXISTS `roast_levels`;
 DROP TABLE IF EXISTS `beans`;
 DROP TABLE IF EXISTS `users`;
