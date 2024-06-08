72cb4598e6fb5dbdd61790c93ba7bb2c3ea0062d
kanazawanao <naomiti3333@gmail.com>
2024-06-08 14:11:42 +0900

    delete

--- openapi/generated/brew-note/go.mod
+++
@@ -1,6 +0,0 @@
-module github.com/brew-note/api
-
-go 1.18
-
-require (
-)
--- src/models/roast_level.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type RoastLevel struct {
-	ID    int    `json:"id"`
-	Level int    `json:"level"`
-	Name  string `json:"name"`
-}
